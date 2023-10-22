cddc2845341783144ff0793e8aff9ea26ffdd325
jymchng <jymchng@hotmail.com>
2023-10-22 22:38:18 +0800

    second

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,9 +4,14 @@ mojo_directory="src"
 original_dir=$(pwd)  # Store the original directory
 
 find "$mojo_directory" -type f -name '*.mojo' | while IFS= read -r mojo_file; do
-    mojo_dir=$(dirname "$mojo_file")
+    # Extract the filename without the path
     file_name=$(basename "$mojo_file")
-    cd "$mojo_dir" || exit 1
-    mojo "$file_name"
-    cd "$original_dir"  # Return to the original directory
+    
+    # Check if the filename is not "__init__.mojo"
+    if [ "$file_name" != "__init__.mojo" ]; then
+        mojo_dir=$(dirname "$mojo_file")
+        cd "$mojo_dir" || exit 1
+        mojo "$file_name"
+        cd "$original_dir"  # Return to the original directory
+    fi
 done
